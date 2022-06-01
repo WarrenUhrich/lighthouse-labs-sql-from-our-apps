ad3adee979cf977866796c931118e11e5b05111f
Warren <hello@warren.codes>
2022-06-01 11:15:03 -0600

    Add .env to gitignore, finish index.js main functionality.

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
+require('dotenv').config();
+
+const { application } = require('express');
 const express = require('express');
 const morgan = require('morgan');
+const villainQueries = require('./database/villain-queries');
+const {getVillains, getVillainById} = villainQueries;
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+// app.set('view engine', 'ejs');
 
 // Middleware.
 app.use(morgan('dev'));
 
+// Routes.
 
+app.get('/villains', (req, res) => {
+    getVillains()
+        .then((villains) => {
+            res.json(villains);
+            // res.render('villains/index', {villains});
+        });
+});
+
+app.get('/villains/:id', (req, res) => {
+    getVillainById(req.params.id)
+        .then((villain) => {
+            res.json(villain);
+            // res.render('villains/show', {villain});
+        });
+});
 
 // Listener.
 app.listen(port, () => {
