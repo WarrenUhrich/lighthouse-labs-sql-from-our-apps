c0310e20ce58c01dfc14f187e2effba8e60f7ebd
Warren <hello@warren.codes>
2022-06-01 09:25:37 -0600

    Add help command to cli villain program.

--- cli-villains.js
+++ cli-villains.js
@@ -18,3 +18,19 @@ client.query('SELECT * FROM movie_villains;')
         console.log(response.rows);
         client.end();
     });
+
+// node cli-villains.js VERB
+const verb = process.argv[2];
+
+switch (verb) {
+    case 'help':
+        console.log(
+            'Help for Villain Command-Line Program:\n',
+            '\tnode cli-villains.js index` # List all villains.\n',
+            '\tnode cli-villains.js show <id>` # Show specific villain.\n',
+            '\tnode cli-villains.js edit <id> <name> <movie>` # Edit specific villain.\n',
+            '\tnode cli-villains.js new <name> <movie>` # New villain.\n',
+            '\tnode cli-villains.js delete <id>` # Remove specific villain.\n',
+        );
+        break;
+}
