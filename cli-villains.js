54800ce8b58c28f7e748ee5122ade1f76dad2d7b
Warren <hello@warren.codes>
2022-06-29 08:57:11 -0600

    Add DB connection via pg.

--- cli-villains.js
+++ cli-villains.js
@@ -1,1 +1,17 @@
 const pg = require('pg');
+
+const Client = pg.Client;
+
+const config = {
+    host: 'localhost',
+    user: 'postgres',
+    password: 'dbuser',
+    database: 'sql_from_apps',
+    port: 5432
+};
+
+const client = new Client(config);
+
+client.connect();
+
+// client.query();
