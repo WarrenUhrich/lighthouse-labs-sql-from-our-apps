16da66aa77275687d3518b0d04b95b47b3046ed6
Warren <hello@warren.codes>
2022-08-02 11:42:58 -0600

    Add index case for cli villain app.

--- cli-villains.js
+++ cli-villains.js
@@ -12,18 +12,6 @@ const client = new Client(config);
 
 client.connect();
 
-// client.query('SELECT * FROM movie_villains;')
-//     .then((result) => {
-//         // console.log(result);
-//         console.log(result.rows);
-//         client.end(); // Frees up the command-line; avoid in Express apps.
-//     })
-//     .catch((error) => {
-//         console.error(error);
-//     });
-
-client.end();
-
 const verb = process.argv[2];
 
 switch (verb) {
@@ -36,8 +24,17 @@ switch (verb) {
             '\tnode cli-villains.js new <villain> <movie> # Create new entry!\n',
             '\tnode cli-villains.js delete <id> # Remove this entry!\n',
         );
+        client.end();
         break;
     case 'index':
-
+        client.query('SELECT * FROM movie_villains;')
+            .then((result) => {
+                // console.log(result);
+                console.log(result.rows);
+                client.end(); // Frees up the command-line; avoid in Express apps.
+            })
+            .catch((error) => {
+                console.error(error);
+            });
         break;
 }
