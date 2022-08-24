56c791557c444fae504f1693081d0b493842bc98
Warren <hello@warren.codes>
2022-08-24 10:11:36 -0600

    Add Express app

--- index.js
+++ index.js
@@ -2,3 +2,15 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+
+app.use(morgan('dev'));
+app.use(bodyParser());
+
+app.listen(PORT, () => {
+    console.log('Express app is now listening on port:', PORT);
+});
