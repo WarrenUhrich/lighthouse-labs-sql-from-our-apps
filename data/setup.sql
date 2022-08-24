1c73d5203eaad5c54436431dcfb4f9aca3519f39
Warren <hello@warren.codes>
2022-08-24 08:31:06 -0600

    Add setup SQL

---
+++ data/setup.sql
@@ -0,0 +1,22 @@
+DROP DATABASE IF EXISTS sql_from_our_apps;
+CREATE DATABASE sql_from_our_apps;
+USE DATABASE sql_from_our_apps;
+
+-- CREATE USER myuser;
+-- ALTER USER myuser password 'your_pass';
+-- GRANT ALL PRIVILEGES ON DATABASE sql_from_our_apps TO myuser;
+
+DROP TABLE IF EXISTS movie_villains;
+
+CREATE TABLE movie_villains (
+  id SERIAL PRIMARY KEY,
+  villain VARCHAR(50),
+  movie VARCHAR(50)
+);
+
+INSERT INTO movie_villains (villain, movie)
+VALUES ('Agent Smith',              'The Matrix'),
+       ('Voldemort',                'Harry Potter Series'),
+       ('Wicked Witch of the West', 'Wizard of Oz'),
+       ('Thanos',                   'Avengers'),
+       ('Doc Ock',                  'Spider-Man');
