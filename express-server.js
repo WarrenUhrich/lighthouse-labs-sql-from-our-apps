c39a2fd1d8615df6dee23d76376365e59f06c658
Warren <hello@warren.codes>
2022-12-06 13:16:13 -0700

    Add express queries

--- express-server.js
+++ express-server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const { getAllVillains, getVillain } = require('./data/villain-queries');
 
 const PORT = 8080;
 const app = express();
@@ -12,4 +13,29 @@ app.listen(PORT, () => {
 
 app.get('/', (req, res) => {
     res.end('Hello, World!');
+});
+
+/**
+ * Villains index
+ */
+app.get('/villains', (req, res) => {
+    getAllVillains()
+        .then((rows) => {
+            res.json(rows);
+        });
+});
+
+/**
+ * Villain show
+ */
+ app.get('/villains/:id', (req, res) => {
+    const id = req.params.id;
+    getVillain(id)
+        .then((row) => {
+            res.json(row);
+        })
+        .catch((error) => {
+            res.status(404);
+            res.json({error: 'Villain not found.'});
+        });
 });
