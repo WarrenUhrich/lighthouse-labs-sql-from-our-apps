5a9918272dae42d1ca0e1432202eadb69f355310
Warren <hello@warren.codes>
2023-06-06 12:36:20 -0600

    Add rest of CRUD; protect against SQL injection

--- movie-villains.js
+++ movie-villains.js
@@ -19,7 +19,70 @@ const args = process.argv.slice(2);
 const command = args[0];
 // console.log(args);
 
-if(args.length === 0) {
+if('index' === command) {
+    client.query('SELECT * FROM movie_villains;')
+        .then((dbResponse) => {
+            // console.log('dbResponse:', dbResponse);
+            console.log(dbResponse.rows);
+            client.end();
+        });
+}else if('create' === command) {
+    const name = args[1];
+    const movie = args[2];
+
+    client.query(`
+        INSERT INTO movie_villains(name, movie)
+        VALUES('$1', '$2');
+    `, [name, movie])
+        .then((dbResponse) => {
+            // console.log('dbResponse:', dbResponse);
+            console.log('A new villain is born!');
+            client.end();
+        });
+}else if('read' === command) {
+    const id = args[1];
+
+    client.query(`
+        SELECT *
+        FROM movie_villains
+        WHERE id=$1;
+    `, [id])
+        .then((dbResponse) => {
+            // console.log('dbResponse:', dbResponse);
+            // Retrieve only the first row/entry
+            // * there should only be ONE matching ID
+            console.log(dbResponse.rows[0]);
+            client.end();
+        });
+}else if('update' === command) {
+    const id = args[1];
+    const name = args[2];
+    const movie = args[3];
+
+    client.query(`
+        UPDATE movie_villains
+        SET
+            name='$2',
+            movie='$3'
+        WHERE id=$1;
+    `, [id, name, movie])
+        .then((dbResponse) => {
+            console.log('This villain has been transformed!');
+            client.end();
+        });
+}else if('delete' === command) {
+    const id = args[1];
+    // console.log(args);
+
+    client.query(`
+        DELETE FROM movie_villains
+        WHERE id=$1;
+    `, [id])
+        .then((dbResponse) => {
+            console.log('Villain defeated!');
+            client.end();
+        });
+}else{
     console.log(`
         MOVIE VILLAINS DB CLI PROGRAM COMMANDS:
         =======================================
@@ -40,11 +103,4 @@ if(args.length === 0) {
                 remove villain
     `);
     client.end();
-}else if('index' === command) {
-    client.query('SELECT * FROM movie_villains;')
-        .then((dbResponse) => {
-            // console.log('dbResponse:', dbResponse);
-            console.log(dbResponse.rows);
-            client.end();
-        });
 }
